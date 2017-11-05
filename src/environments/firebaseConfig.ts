a96129af36a66e96e314341dfb0fe781b3e8c8f8
Eduardo Elias Saleh <eduardo@lab15.io>
2017-11-05 20:37:11 +0100

    Initial commit by ngx-firebase-bootstrap generator

---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,11 @@
+
+
+export const firebaseConfig = {
+    // Paste all this from the Firebase console...
+    apiKey: "AIzaSyCQNS_494LMsv03ZMEHxXdi1Qtk2POkLps",
+    authDomain: "jspoker-b43d2.firebaseapp.com",
+    databaseURL: "https://jspoker-b43d2.firebaseio.com/",
+    storageBucket: "",
+    messagingSenderId: "115305338810 "
+};
+
